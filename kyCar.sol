4adee4bf4dd9199f553becd74bf75a03b1ffca95
Chrysostomos Daniel <daniel.chrysostomos@gmail.com>
2018-01-27 00:46:36 +0200

    Fixed errors and add investors get user car details

--- kyCar.sol
+++ kyCar.sol
@@ -1,6 +1,6 @@
 contract KYCar is Ownable, KYC {
     mapping(address => CarDetail[]) carDetails;
-    address[] personAccounts;
+    address[] personCarAccounts;
 
     struct CarDetail {
         bytes8 license_plate;
@@ -15,10 +15,16 @@ contract KYCar is Ownable, KYC {
         bytes1 status;
     }
 
+    modifier carPersonHaveCars(uint index) {
+        require (index <= carDetails[msg.sender].length);
+        _;
+    } 
+
     modifier AdminSenderHavecars(address sender, uint index) {
         require (index <= carDetails[sender].length);
         _;
     }
+    
     
     function addCarDetails (
         bytes8 license_plate,
@@ -49,7 +55,7 @@ contract KYCar is Ownable, KYC {
         }));
     
         if (carDetails[msg.sender].length == 1) {
-            personAccounts.push(msg.sender);
+            personCarAccounts.push(msg.sender);
         }
         
         return true;
@@ -80,7 +86,11 @@ contract KYCar is Ownable, KYC {
             carDetails[msg.sender][index].year
         );
     }
-    
+
+    function getAdminCarCount(address sender) onlyOwner public constant returns(uint count) {
+        return carDetails[sender].length;
+    }
+
     function getCarAmounts(uint index) public constant returns(
         uint deposit,
         uint amount_per_day
@@ -92,11 +102,94 @@ contract KYCar is Ownable, KYC {
         );
     }
 
-    function getAdminCarDetails(address sender, uint index) AdminSenderHavecars(sender, index) onlyOwner public constant returns(
-        bytes8 license_plate
+    function setAdminCarStatus(address sender, uint index,bytes1 status,uint deposit, uint amount_per_day) AdminSenderHavecars(sender, index) onlyOwner returns(bool) {
+        require(status == 'A' || status == 'R' || status == 'P');
+
+        carDetails[sender][index].status = status;
+        carDetails[sender][index].deposit = deposit;
+        carDetails[sender][index].amount_per_day = amount_per_day;    
+
+        return true;
+    }
+
+   function getAdminCarDetails(address sender, uint index) AdminSenderHavecars(sender, index) onlyOwner public constant returns(
+        bytes8 license_plate,
+        bytes32 brand,
+        bytes32 model,
+        bytes32 category
     ) {
         return (
             carDetails[sender][index].license_plate,
+            carDetails[sender][index].brand, 
+            carDetails[sender][index].model,
+            carDetails[sender][index].category
+        );
+    }
+    
+    function getAdminCarExtra(address sender, uint index) AdminSenderHavecars(sender, index) onlyOwner public constant returns(
+        bytes4 engine_size,
+        bytes8 horse_power,
+        uint year
+    ) {
+        return (
+            carDetails[sender][index].engine_size,
+            carDetails[sender][index].horse_power,
+            carDetails[sender][index].year
+        );
+        
+    }
+
+    function getAdminCarAmounts(address sender, uint index) AdminSenderHavecars(sender, index) onlyOwner public constant returns(
+        uint deposit,
+        uint amount_per_day
+    ) {
+        return ( 
+            carDetails[sender][index].deposit,
+            carDetails[sender][index].amount_per_day
+        );
+    }
+    
+    function getAdminCarPersons() onlyOwner view public returns (address[]) {
+        return personCarAccounts;
+    }
+
+     function getCarStatus(uint index) public constant returns(bytes1 status) {
+        require(index <= carDetails[msg.sender].length);
+
+        return carDetails[msg.sender][index].status;
+    }
+
+    function getAdminCarStatus(address sender, uint index) AdminSenderHavecars(sender, index) AdminSenderHavecars(sender, index) onlyOwner returns(bytes1) {
+        return carDetails[sender][index].status;
+    }
+
+     function getInvestorCarCount(address sender) carPersonHaveCars public constant returns(uint count) {
+        return carDetails[sender].length;
+    }
+    
+    function getInvestorCarAmounts(address sender, uint index) carPersonHaveCars public constant returns(
+        uint deposit,
+        uint amount_per_day
+    ) {
+        
+        return(
+            carDetails[sender][index].deposit,
+            carDetails[sender][index].amount_per_day
+        );
+    }
+
+    function getInvestorCarDetails(address sender, uint index) carPersonHaveCars public constant returns(
+        bytes32 brand,
+        bytes32 model,
+        bytes32 category,
+        bytes4 engine_size,
+        bytes8 horse_power,
+        uint year
+
+    ) {
+        require(index <= carDetails[sender].length && carDetails[sender][index].status == 'A');
+
+        return (
             carDetails[sender][index].brand, 
             carDetails[sender][index].model,
             carDetails[sender][index].category, 
@@ -104,21 +197,6 @@ contract KYCar is Ownable, KYC {
             carDetails[sender][index].horse_power,
             carDetails[sender][index].year
         );
-    }
 
-    function getAdminCarAmounts(address sender, uint index) AdminSenderHavecars(sender, index) onlyOwner public constant returns(
-        uint deposit,
-        uint amount_per_day
-    ) {
-        require(index <= carDetails[msg.sender].length);
-       return ( 
-        carDetails[sender][index].deposit,
-        carDetails[sender][index].amount_per_day
-        );
     }
-    
-    function getAdminSenders() onlyOwner view public returns (address[]) {
-        return personAccounts;
-    }
-    
 }
